0d1507a71535a2a83a934c852a50c7e88655fb9c
Safura234 <safuramg@code.edu.az>
2022-05-17 13:22:58 +0400

    https://github.com/Safura234/Try-catch-example2.git

---
+++ Example 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Example_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+                
+                {
+                    string name = null;
+                    Console.WriteLine("Name: ");
+                    if (name == null);
+
+                }
+            catch
+            {
+                Console.Write("Error");
+            }
+            finally
+            {
+                Console.Write("Re - Enter your name");
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
+
